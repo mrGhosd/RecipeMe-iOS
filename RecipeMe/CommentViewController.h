7b2bd454b1bd8ff0cf2b026946a4338990cef473
vsokoltsov <vforvad@gmail.com>
2015-07-08 21:31:59 +0300

    add view for comment edit view

---
+++ RecipeMe/CommentViewController.h
@@ -0,0 +1,13 @@
+//
+//  CommentViewController.h
+//  RecipeMe
+//
+//  Created by vsokoltsov on 08.07.15.
+//  Copyright (c) 2015 vsokoltsov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface CommentViewController : ViewController
+
+@end
