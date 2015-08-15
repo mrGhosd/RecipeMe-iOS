541d9028d4b717dff67f0ed8686189135338fed1
vsokoltsov <vforvad@gmail.com>
2015-08-15 23:37:10 +0300

    implement prototype of user profile view cell

---
+++ RecipeMe/UserListTableViewCell.h
@@ -0,0 +1,19 @@
+//
+//  UserListTableViewCell.h
+//  RecipeMe
+//
+//  Created by vsokoltsov on 15.08.15.
+//  Copyright (c) 2015 vsokoltsov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserListTableViewCell : UITableViewCell
+@property (strong, nonatomic) IBOutlet UIImageView *userAvatar;
+@property (strong, nonatomic) IBOutlet UILabel *userName;
+@property (strong, nonatomic) IBOutlet UILabel *recipesCount;
+@property (strong, nonatomic) IBOutlet UILabel *commentsCount;
+@property (strong, nonatomic) IBOutlet UILabel *followersCount;
+@property (strong, nonatomic) IBOutlet UILabel *followingCount;
+
+@end
---
+++ RecipeMe/UserListTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  UserListTableViewCell.m
+//  RecipeMe
+//
+//  Created by vsokoltsov on 15.08.15.
+//  Copyright (c) 2015 vsokoltsov. All rights reserved.
+//
+
+#import "UserListTableViewCell.h"
+
+@implementation UserListTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
